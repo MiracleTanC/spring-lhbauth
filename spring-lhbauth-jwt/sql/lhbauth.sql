592e03ac3a219f0dc246e61e781347dfc10bb388
lvhaibao <lvhaibao@sunniwell.net>
2018-12-25 15:18:13 +0800

    实现简单的ouath2服务

---
+++ spring-lhbauth-jwt/sql/lhbauth.sql
@@ -0,0 +1,34 @@
+/*
+SQLyog Professional v12.08 (64 bit)
+MySQL - 5.6.39 : Database - lhbauth
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`lhbauth` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `lhbauth`;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL COMMENT '自增长ID',
+  `username` varchar(16) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(16) DEFAULT NULL COMMENT '密码',
+  `mobile` varchar(16) DEFAULT NULL COMMENT '手机号',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ spring-lhbauth-jwt/src/main/java/com/lhb/lhbauth/jwt/demo/config/MyWebSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.lhb.lhbauth.jwt.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author lvhaibao
+ * @description 浏览器配置
+ * @date 2018/12/25 0025 10:53
+ */
+@Configuration
+public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                //表单登录
+                .formLogin()
+                //允许访问
+                .and().authorizeRequests().antMatchers("/oauth/**,", "hello").permitAll().anyRequest().authenticated()
+                //禁用跨站伪造
+                .and().csrf().disable();
+
+    }
+
+
+}
