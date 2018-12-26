9a99e050b685df563825493bff5a4694e1f12eaf
lvhaibao <lvhaibao@sunniwell.net>
2018-12-26 18:46:24 +0800

    自定义登录和授权页面

--- spring-lhbauth-jwt/src/main/java/com/lhb/lhbauth/jwt/demo/config/MyWebSecurityConfig.java
+++ spring-lhbauth-jwt/src/main/java/com/lhb/lhbauth/jwt/demo/config/MyWebSecurityConfig.java
@@ -1,11 +1,12 @@
 package com.lhb.lhbauth.jwt.demo.config;
 
+import com.lhb.lhbauth.jwt.demo.constants.FromLoginConstant;
+import com.lhb.lhbauth.jwt.demo.properties.SecurityProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,6 +18,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  */
 @Configuration
 public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private SecurityProperties securityProperties;
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -32,10 +37,15 @@ public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                //表单登录
-                .formLogin()
+                //表单登录,loginPage为登录请求的url,loginProcessingUrl为表单登录处理的URL
+                .formLogin().loginPage(FromLoginConstant.LOGIN_PAGE).loginProcessingUrl(FromLoginConstant.LOGIN_PROCESSING_URL)
                 //允许访问
-                .and().authorizeRequests().antMatchers("/user/hello").permitAll().anyRequest().authenticated()
+                .and().authorizeRequests().antMatchers(
+                "/user/hello",
+                FromLoginConstant.LOGIN_PROCESSING_URL,
+                FromLoginConstant.LOGIN_PAGE,
+                securityProperties.getOauthLogin().getOauthLogin(),
+                securityProperties.getOauthLogin().getOauthGrant()).permitAll().anyRequest().authenticated()
                 //禁用跨站伪造
                 .and().csrf().disable();
 
